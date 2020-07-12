de33f5e7c641278a434f4daf339386927e56cd08
virginia <43097333+virginiaritonga@users.noreply.github.com>
2020-07-12 10:43:57 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer'
 import App from './App';
+import Home from './views/Home'
+import Feedback from './views/Feedback'
 
-test('renders learn ', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('fetch advice', () => {
+  const component = renderer.create(
+    <Home />
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapShot();
+
+  component.getInstance()._handleNewAdvice();
+  tree = component.toJSON();
+  expect(tree).toMatchSnapShot();
+})
+
+test('change name', () => {
+  const component = renderer.create(
+    <Feedback />
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapShot();
+
+  component.getInstance().nameChanged();
+  tree = component.toJSON();
+  expect(tree).toMatchSnapShot();
+})
